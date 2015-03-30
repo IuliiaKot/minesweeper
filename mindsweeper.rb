a015f4a68de4d70ff8e910d0a84a4754dda0f4b9
Jeremy-Iuliia <Jeremy-Iuliia@appacademy.io>
2015-03-30 11:44:12 -0400

    make board print

--- mindsweeper.rb
+++ mindsweeper.rb
@@ -1,26 +1,32 @@
 class Board
 
-  attr_reader :height, :width, :bombs
+  attr_reader :height, :width, :bombs, :board
 
   def initialize(height=9, width=9, bombs=10)
     @height = height
     @width = width
     @bombs = bombs
+    @board = make_board
   end
 
 
 
   def make_board
-    board = Array.new(height) {Array.new(width)}
+    board = Array.new(height) {Array.new(width, '*')}
+    10.times do |i|
+      x = rand(0..8)
+      y = rand(0..8)
+      board[x][y] = "b"
+    end
+    board
+  end
 
-
-    board[1][2] = "hey"
-    board[0][0] = "hello"
-
-    p board
+  def print_board
+    board.each do |line|
+      p line.join(' ')
+    end
   end
 end
 
 board = Board.new
-
-board.make_board
+board.print_board
