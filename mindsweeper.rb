aebbbf6186e3a3cb324edc84cb6e1656c5116899
Jeremy-Iuliia <Jeremy-Iuliia@appacademy.io>
2015-03-30 12:02:13 -0400

    add find neighbor squares method

--- mindsweeper.rb
+++ mindsweeper.rb
@@ -28,5 +28,45 @@ class Board
   end
 end
 
+class Square
+
+  attr_reader :pos, :bomb, :flag, :chosen
+
+  NEIGHBORS = [
+    [-1, -1],
+    [-1, 0],
+    [1,  1],
+    [1,  0],
+    [0,  1],
+    [-1, 1],
+    [1, -1],
+    [0, -1]
+  ]
+
+  def initialize(pos, bomb, chosen = false, flag = false)
+    @pos = pos
+    @bomb =  bomb
+    @chosen = chosen
+    @flag = flag
+
+  end
+
+  def neighbor_squares
+    neighbors = []
+    NEIGHBORS.each do |(x, y)|
+      if (pos[0] + x).between?(0,8) && (pos[1] + y).between?(0,8)
+        neighbors << [pos[0] + x, pos[1] + y]
+      end
+    end
+    neighbors
+  end
+end
+
+
+
 board = Board.new
 board.print_board
+
+square = Square.new([0,0], true)
+
+p square.neighbor_squares
